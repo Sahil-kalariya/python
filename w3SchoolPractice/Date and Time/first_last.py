2fa786056de2dabb6c6f8e54149e887f842a7a1a
sahil-kalariya <sahil.k@crestskillserve.com>
2025-01-16 13:39:35 +0530

    exercise

---
+++ w3SchoolPractice/Date and Time/first_last.py
@@ -0,0 +1,5 @@
+
+import datetime
+
+print("First Second: ", datetime.time.min)
+print("Last Second: ", datetime.time.max)
--- w3SchoolPractice/Date and Time/parse_string.py
+++ w3SchoolPractice/Date and Time/parse_string.py
@@ -1,6 +1,14 @@
 import time
+import datetime
 
 time_string = "22 January, 2020"
 print("String representing time:", time_string)
 result = time.strptime(time_string, "%d %B, %Y")
 print(result)
+
+
+
+# Print the minimum time (midnight) in the datetime module
+print("First Second: ", datetime.time.min)
+# Print the maximum time (11:59:59.999999) in the datetime module
+print("Last Second: ", datetime.time.max)
